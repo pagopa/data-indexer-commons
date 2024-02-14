f0633464eb2e60d8242a36897512202685ae4ec3
Davide Marro <davide.marro@pagopa.it>
2024-02-14 11:58:41 +0100

    Fixing tests

--- __integrations__/__tests__/system/configuration.test.ts
+++ __integrations__/__tests__/system/configuration.test.ts
@@ -2,36 +2,95 @@ import * as E from "fp-ts/Either";
 import { pipe } from "fp-ts/lib/function";
 import * as fs from "fs";
 import { JSONService } from "../../../src/configuration/service";
-import { validMinimumConfiguration } from "./files";
+import {
+  anInvalidDataEnrichment,
+  anInvalidDataFiltering,
+  anInvalidDataMapping,
+  anInvalidDataOutput,
+  anInvalidDataSourcing,
+  missingDataOutput,
+  missingDataSourcing,
+  validConfigurationWithEnrichment,
+  validConfigurationWithFiltering,
+  validConfigurationWithMapping,
+  validFullConfiguration,
+  validMinimumConfiguration,
+} from "./files";
+
+export const configurations = [
+  {
+    name: "validMinimumConfiguration",
+    configuration: validMinimumConfiguration,
+  },
+  {
+    name: "validConfigurationWithMapping",
+    configuration: validConfigurationWithMapping,
+  },
+  {
+    name: "validConfigurationWithEnrichment",
+    configuration: validConfigurationWithEnrichment,
+  },
+  {
+    name: "validConfigurationWithFiltering",
+    configuration: validConfigurationWithFiltering,
+  },
+  { name: "validFullConfiguration", configuration: validFullConfiguration },
+  { name: "missingDataSourcing", configuration: missingDataSourcing },
+  { name: "missingDataOutput", configuration: missingDataOutput },
+  { name: "anInvalidDataSourcing", configuration: anInvalidDataSourcing },
+  { name: "anInvalidDataOutput", configuration: anInvalidDataOutput },
+  { name: "anInvalidDataEnrichment", configuration: anInvalidDataEnrichment },
+  { name: "anInvalidDataMapping", configuration: anInvalidDataMapping },
+  { name: "anInvalidDataFiltering", configuration: anInvalidDataFiltering },
+];
 
 beforeAll(() => {
-  const fileContent = JSON.stringify(validMinimumConfiguration);
-  fs.writeFileSync(
-    `${__dirname}/files/validMinimumConfiguration.json`,
-    fileContent,
-  );
+  configurations.forEach(({ name, configuration }) => {
+    fs.writeFileSync(
+      `${__dirname}/${name}.json`,
+      JSON.stringify(configuration),
+    );
+  });
 });
 
 afterAll(() => {
-  fs.unlinkSync(`${__dirname}/files/validMinimumConfiguration.json`);
+  configurations.forEach(({ name }) => {
+    fs.unlinkSync(`${__dirname}/${name}.json`);
+  });
 });
 
 describe("configuration", () => {
-  it("should decode a valid configuration file", async () => {
+  it.each`
+    description                                                          | filename                              | configuration                       | success
+    ${"should decode properly with minimum valid config"}                | ${"validMinimumConfiguration"}        | ${validMinimumConfiguration}        | ${true}
+    ${"should decode properly with valid configuration with mapping"}    | ${"validConfigurationWithMapping"}    | ${validConfigurationWithMapping}    | ${true}
+    ${"should decode properly with valid configuration with enrichment"} | ${"validConfigurationWithEnrichment"} | ${validConfigurationWithEnrichment} | ${true}
+    ${"should decode properly with valid configuration with filtering"}  | ${"validConfigurationWithFiltering"}  | ${validConfigurationWithFiltering}  | ${true}
+    ${"should decode properly with valid full configuration"}            | ${"validFullConfiguration"}           | ${validFullConfiguration}           | ${true}
+    ${"should fail with missing data sourcing"}                          | ${"missingDataSourcing"}              | ${missingDataSourcing}              | ${false}
+    ${"should fail with missing output"}                                 | ${"missingDataOutput"}                | ${missingDataOutput}                | ${false}
+    ${"should fail with invalid data sourcing"}                          | ${"anInvalidDataSourcing"}            | ${anInvalidDataSourcing}            | ${false}
+    ${"should fail with invalid data output"}                            | ${"anInvalidDataOutput"}              | ${anInvalidDataOutput}              | ${false}
+    ${"should fail with invalid data enrichment"}                        | ${"anInvalidDataEnrichment"}          | ${anInvalidDataEnrichment}          | ${false}
+    ${"should fail with invalid data mapping"}                           | ${"anInvalidDataMapping"}             | ${anInvalidDataMapping}             | ${false}
+    ${"should fail with invalid data filtering"}                         | ${"anInvalidDataFiltering"}           | ${anInvalidDataFiltering}           | ${false}
+  `("$description", ({ filename, configuration, success }) => {
     return pipe(
       E.Do,
       E.bind("service", () => E.right(JSONService)),
       E.chain(({ service }) =>
         pipe(
-          service.read(`${__dirname}/files/validMinimumConfiguration.json`),
+          service.read(`${__dirname}/${filename}.json`),
           E.chain((json) => service.decode(json)),
         ),
       ),
-      E.bimap(
-        (err) => {
-          throw new Error(`Error during JSON decoding: ${err.message}`);
+      E.fold(
+        () => {
+          expect(success).toBeFalsy();
         },
-        (data) => expect(data).toEqual(validMinimumConfiguration),
+        (data) => {
+          expect(data).toEqual(configuration);
+        },
       ),
     );
   });
--- __integrations__/__tests__/system/files.ts
+++ __integrations__/__tests__/system/files.ts
@@ -21,3 +21,318 @@ export const validMinimumConfiguration = {
     deduplicationStrategy: "TIMESTAMP",
   },
 };
+
+export const validConfigurationWithMapping = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {
+    mapper: "MERGE_FIELDS",
+    type: "MULTIPLE_INPUT",
+    newFieldName: "mergedField",
+    inputOutputFields: [
+      {
+        inputFieldName: "foo",
+        outputFieldName: "bar",
+      },
+    ],
+    separator: "-",
+  },
+  dataEnrichment: {},
+  dataFiltering: {},
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "DATA_OUTPUT",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
+
+export const validConfigurationWithEnrichment = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {},
+  dataEnrichment: {
+    params: {
+      blobFilename: "file.txt",
+      connectionString: "blob_connection_string",
+      containerName: "container",
+      extension: "txt",
+    },
+    type: "BlobStorage",
+  },
+  dataFiltering: {},
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "DATA_OUTPUT",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
+
+export const validConfigurationWithFiltering = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {},
+  dataEnrichment: {},
+  dataFiltering: {
+    dataFiltering: { fieldName: "age", condition: "gte", staticValue: 18 },
+  },
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "DATA_OUTPUT",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
+
+export const validFullConfiguration = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {
+    mapper: "MERGE_FIELDS",
+    type: "MULTIPLE_INPUT",
+    newFieldName: "mergedField",
+    inputOutputFields: [
+      {
+        inputFieldName: "foo",
+        outputFieldName: "bar",
+      },
+    ],
+    separator: "-",
+  },
+  dataEnrichment: {
+    params: {
+      blobFilename: "file.txt",
+      connectionString: "blob_connection_string",
+      containerName: "container",
+      extension: "txt",
+    },
+    type: "BlobStorage",
+  },
+  dataFiltering: {
+    dataFiltering: { fieldName: "age", condition: "gte", staticValue: 18 },
+  },
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "DATA_OUTPUT",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
+
+export const missingDataSourcing = {
+  dataSourcing: {},
+  dataMapping: {},
+  dataEnrichment: {},
+  dataFiltering: {},
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "DATA_OUTPUT",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
+
+export const missingDataOutput = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {},
+  dataEnrichment: {},
+  dataFiltering: {},
+  dataOutput: {},
+};
+
+export const anInvalidDataSourcing = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "INVALID",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {},
+  dataEnrichment: {},
+  dataFiltering: {},
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "DATA_OUTPUT",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
+
+export const anInvalidDataOutput = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {},
+  dataEnrichment: {},
+  dataFiltering: {},
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "INVALID",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
+
+export const anInvalidDataEnrichment = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {},
+  dataEnrichment: {
+    params: {
+      blobFilename: "file.txt",
+      connectionString: "blob_connection_string",
+      containerName: "container",
+      extension: "txt",
+    },
+    type: "INVALID",
+  },
+  dataFiltering: {},
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "DATA_OUTPUT",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
+
+export const anInvalidDataMapping = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {
+    mapper: "MERGE_FIELDS",
+    type: "INVALID",
+    newFieldName: "mergedField",
+    inputOutputFields: [
+      {
+        inputFieldName: "foo",
+        outputFieldName: "bar",
+      },
+    ],
+    separator: "-",
+  },
+  dataEnrichment: {},
+  dataFiltering: {},
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "DATA_OUTPUT",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
+
+export const anInvalidDataFiltering = {
+  dataSourcing: {
+    connectionString: "foo",
+    type: "DB",
+    dbType: "CosmosDB",
+    sourceType: "CDC",
+    props: {
+      collectionName: "collName",
+      dbName: "dbName",
+      leaseContainerName: "lease",
+    },
+  },
+  dataMapping: {},
+  dataEnrichment: {},
+  dataFiltering: {
+    dataFilteringInvalid: {
+      fieldName: "age",
+      condition: "gte",
+      staticValue: 18,
+    },
+  },
+  dataOutput: {
+    connectionString: "connectionString",
+    indexName: "indexName",
+    type: "DATA_OUTPUT",
+    indexer: "ELASTICSEARCH",
+    deduplicationStrategy: "TIMESTAMP",
+  },
+};
