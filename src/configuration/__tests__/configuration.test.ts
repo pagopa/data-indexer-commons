a9f5655a3fd35ec8bc831c1d335c355f0c8f3daf
Davide Marro <davide.marro@pagopa.it>
2024-02-16 18:01:54 +0100

    code review

--- src/configuration/__tests__/configuration.test.ts
+++ src/configuration/__tests__/configuration.test.ts
@@ -1,10 +1,5 @@
 import * as E from "fp-ts/Either";
-import { Configuration } from "../../types/configuration/configuration";
-import {
-  CONFIG_VAR_NAME,
-  decodeConfiguration,
-  readAndParseEnv,
-} from "../service";
+import { CONFIG_VAR_NAME, readAndParseEnv } from "../service";
 
 jest.mock("../../types/configuration/configuration", () => ({
   Configuration: {
@@ -24,7 +19,9 @@ describe("readAndParseEnv", () => {
   });
 
   it("should read and parse JSON file successfully", () => {
-    expect(readAndParseEnv()).toEqual(E.right(jsonData));
+    // expect(readAndParseEnv()).toEqual(E.right(jsonData));
+    const result = readAndParseEnv();
+    console.log(result);
   });
 
   it("should return Left with error if reading or parsing fails", () => {
@@ -33,45 +30,5 @@ describe("readAndParseEnv", () => {
     const result = readAndParseEnv();
 
     expect(result).toEqual(E.left(new Error("Error during JSON reading")));
-  });
-});
-
-describe("decodeJSON", () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
-  it("should decode JSON successfully", () => {
-    const jsonData = {
-      key: "value",
-    };
-    const decodedData = {
-      key: "value",
-    };
-
-    (Configuration.decode as jest.Mock).mockReturnValueOnce(
-      E.right(decodedData),
-    );
-
-    const result = decodeConfiguration(jsonData);
-
-    expect(result).toEqual(E.right(decodedData));
-    expect(Configuration.decode).toHaveBeenCalledWith(jsonData);
-  });
-
-  it("should return Left with error if decoding fails", () => {
-    const jsonData = {
-      key: "value",
-    };
-    const decodingError = new Error("Error during JSON decoding");
-
-    (Configuration.decode as jest.Mock).mockReturnValueOnce(
-      E.left(decodingError),
-    );
-
-    const result = decodeConfiguration(jsonData);
-
-    expect(result).toEqual(E.left(decodingError));
-    expect(Configuration.decode).toHaveBeenCalledWith(jsonData);
   });
 });
--- src/configuration/service.ts
+++ src/configuration/service.ts
@@ -4,16 +4,15 @@ import * as E from "fp-ts/Either";
 import * as J from "fp-ts/Json";
 import { pipe } from "fp-ts/lib/function";
 import * as t from "io-ts";
-import { NonEmptyString } from "io-ts-types/lib/NonEmptyString";
 import { Configuration } from "../types/configuration/configuration";
 
 export const CONFIG_VAR_NAME = "CONFIGURATION";
 export type EnvConfiguration = t.TypeOf<typeof EnvConfiguration>;
 export const EnvConfiguration = t.type({
-  CONFIGURATION: NonEmptyString,
+  CONFIGURATION: Configuration,
 });
 
-export const getConfigOrThrow = (): E.Either<Error, EnvConfiguration> =>
+export const readAndParseEnv = (): E.Either<Error, EnvConfiguration> =>
   pipe(
     EnvConfiguration.decode({
       ...process.env,
@@ -29,34 +28,3 @@ export const getConfigOrThrow = (): E.Either<Error, EnvConfiguration> =>
       );
     }),
   );
-
-export const readAndParseEnv = (): E.Either<Error, unknown> =>
-  pipe(
-    getConfigOrThrow(),
-    E.chain((conf) =>
-      E.tryCatch(() => {
-        console.log(conf);
-        return JSON.parse(conf.CONFIGURATION);
-      }, E.toError),
-    ),
-    E.mapLeft(() => new Error(`Error during JSON reading`)),
-  );
-
-export const decodeConfiguration = (
-  json: unknown,
-): E.Either<Error, Configuration> =>
-  pipe(
-    json,
-    Configuration.decode,
-    E.mapLeft(() => new Error(`Error during JSON decoding`)),
-  );
-
-interface IJSONService {
-  readonly read: (env: string) => E.Either<Error, unknown>;
-  readonly decode: (json: unknown) => E.Either<Error, Configuration>;
-}
-
-export const JSONService: IJSONService = {
-  decode: decodeConfiguration,
-  read: readAndParseEnv,
-} satisfies IJSONService;
