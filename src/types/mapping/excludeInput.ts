0da05a1d03293b0b561583fdebebad1639e00e8c
AleDore <alessio85.dore@gmail.com>
2024-03-05 11:54:25 +0100

    [#IOPLT-377] Add support for fields exclusion

---
+++ src/types/mapping/excludeInput.ts
@@ -0,0 +1,22 @@
+import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
+import * as t from "io-ts";
+
+const ExcludeInputConfig = t.type({
+  fields: t.readonlyArray(NonEmptyString),
+  type: t.literal("EXCLUDE_INPUT"),
+});
+
+export const ExcludeFieldsMapping = t.type({
+  mapper: t.literal("EXCLUDE_FIELDS"),
+});
+
+export type ExcludeFieldsMapping = t.TypeOf<typeof ExcludeFieldsMapping>;
+
+export const ExcludeInputExcludeFieldsMapping = t.intersection([
+  ExcludeInputConfig,
+  ExcludeFieldsMapping,
+]);
+
+export const ExcludeInputMapping = ExcludeInputExcludeFieldsMapping;
+
+export type ExcludeInputMapping = t.TypeOf<typeof ExcludeInputMapping>;
