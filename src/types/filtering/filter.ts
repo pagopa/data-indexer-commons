705f9824bbab3febe0cabcd1cc30f61f0eee951d
Cristiano Tofani <cri.tofani@gmail.com>
2024-03-08 12:29:32 +0100

    [IOPLT-287] Update filter model io-ts decoder

--- src/types/filtering/filter.ts
+++ src/types/filtering/filter.ts
@@ -2,23 +2,54 @@
 import { NonEmptyString } from "@pagopa/ts-commons/lib/strings";
 import * as t from "io-ts";
 
-export const DataFilter = t.partial({
+const DataFilterCommon = t.type({
   fieldName: NonEmptyString,
-  condition: t.keyof({
-    eq: null,
-    neq: null,
-    gt: null,
-    gte: null,
-    lt: null,
-    lte: null,
-  }),
-  staticValue: t.union([
-    NonEmptyString,
-    t.number,
-    t.boolean,
-    t.null,
-    t.undefined,
-  ]),
 });
+
+const DataFilterStaticConditions = t.union([
+  t.type({
+    condition: t.union([t.literal("eq"), t.literal("neq")]),
+    staticValue: t.union([NonEmptyString, t.number, t.boolean]),
+  }),
+  t.type({
+    condition: t.union([
+      t.literal("gt"),
+      t.literal("gte"),
+      t.literal("lt"),
+      t.literal("lte"),
+    ]),
+    staticValue: t.number,
+  }),
+  t.type({
+    condition: t.union([
+      t.literal("isNull"),
+      t.literal("isNotNull"),
+      t.literal("isUndefined"),
+      t.literal("isNotUndefined"),
+    ]),
+  }),
+]);
+
+const DataFilterStatic = t.intersection([
+  DataFilterCommon,
+  DataFilterStaticConditions,
+]);
+
+const DataFilterField = t.intersection([
+  DataFilterCommon,
+  t.type({
+    condition: t.union([
+      t.literal("eq"),
+      t.literal("neq"),
+      t.literal("lt"),
+      t.literal("lte"),
+      t.literal("gt"),
+      t.literal("gte"),
+    ]),
+    compareField: NonEmptyString,
+  }),
+]);
+
+export const DataFilter = t.union([DataFilterStatic, DataFilterField]);
 
 export type DataFilter = t.TypeOf<typeof DataFilter>;
