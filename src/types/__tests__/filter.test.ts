9ae50b296dda89327da228476ddedfbeab64fbfc
Cristiano Tofani <cri.tofani@gmail.com>
2024-03-15 15:12:11 +0100

    chore: Adds the filter type attribute to DataFilter model

--- src/types/__tests__/filter.test.ts
+++ src/types/__tests__/filter.test.ts
@@ -4,18 +4,19 @@ import { DataFilter } from "../filtering/filter";
 
 describe("Filter", () => {
   it.each`
-    description                             | input                                                                    | success
-    ${"valid filter"}                       | ${{ fieldName: "age", condition: "gte", staticValue: 18 }}               | ${true}
-    ${"missing properties"}                 | ${{ fieldName: "age" }}                                                  | ${false}
-    ${"invalid params"}                     | ${{ fieldName: "age", condition: "gte", staticValue: "some" }}           | ${false}
-    ${"valid isNull filter"}                | ${{ fieldName: "age", condition: "isNull" }}                             | ${true}
-    ${"invalid isNull filter"}              | ${{ fieldName: "age", condition: "isNull", staticValue: 8 }}             | ${false}
-    ${"invalid condition"}                  | ${{ fieldName: "age", condition: "invalid", staticValue: 18 }}           | ${false}
-    ${"filter with string staticValue"}     | ${{ fieldName: "name", condition: "eq", staticValue: "John Doe" }}       | ${true}
-    ${"filter with number staticValue"}     | ${{ fieldName: "age", condition: "gte", staticValue: 18 }}               | ${true}
-    ${"filter with boolean staticValue"}    | ${{ fieldName: "is_active", condition: "eq", staticValue: true }}        | ${true}
-    ${"valid filter with compareFieldName"} | ${{ fieldName: "is_active", condition: "eq", compareField: "some" }}     | ${true}
-    ${"filter with boolean staticValue"}    | ${{ fieldName: "is_active", condition: "isNull", compareField: "some" }} | ${false}
+    description                             | input                                                                                                          | success
+    ${"valid filter"}                       | ${{ type: "staticFilter", fieldName: "age", condition: "gte", staticValue: 18 }}                               | ${true}
+    ${"missing properties"}                 | ${{ type: "staticFilter", fieldName: "age" }}                                                                  | ${false}
+    ${"invalid params"}                     | ${{ type: "staticFilter", fieldName: "age", condition: "gte", staticValue: "some" }}                           | ${false}
+    ${"valid isNull filter"}                | ${{ type: "staticFilter", fieldName: "age", condition: "isNull" }}                                             | ${true}
+    ${"invalid isNull filter"}              | ${{ type: "staticFilter", fieldName: "age", condition: "isNull", staticValue: 8 }}                             | ${false}
+    ${"invalid condition"}                  | ${{ type: "staticFilter", fieldName: "age", condition: "invalid", staticValue: 18 }}                           | ${false}
+    ${"filter with string staticValue"}     | ${{ type: "staticFilter", fieldName: "name", condition: "eq", staticValue: "John Doe" }}                       | ${true}
+    ${"filter with number staticValue"}     | ${{ type: "staticFilter", fieldName: "age", condition: "gte", staticValue: 18 }}                               | ${true}
+    ${"filter with boolean staticValue"}    | ${{ type: "staticFilter", fieldName: "is_active", condition: "eq", staticValue: true }}                        | ${true}
+    ${"valid filter with compareFieldName"} | ${{ type: "dynamicFilter", fieldName: "is_active", condition: "eq", compareField: "some" }}                    | ${true}
+    ${"filter with boolean staticValue"}    | ${{ type: "dynamicFilter", fieldName: "is_active", condition: "isNull", compareField: "some" }}                | ${false}
+    ${"filter with boolean staticValue"}    | ${{ type: "dynamicFilter", fieldName: "is_active", condition: "eq", staticValue: true, compareField: "some" }} | ${false}
   `("should decode $description", ({ input, success }) => {
     pipe(
       input,
